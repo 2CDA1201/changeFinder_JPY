f7636e0cb1b6b35e93218ad3a0980e4879ace80a
psytoh <uuk65536@gmail.com>
2024-10-18 14:45:34 +0900

    Init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    int payment, price;
+    cout << "Payment >";
+    cin >> payment;
+    cout << "Price >";
+    cin >> price;
+
+    cout << price << "\n" << payment << endl;
+}
