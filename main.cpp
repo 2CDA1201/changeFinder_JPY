c25494943ca63a518d10fbe436643510181088ea
psytoh <uuk65536@gmail.com>
2024-10-18 15:23:05 +0900

    finished

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+#include <array>
 
 int main() {
     using namespace std;
 
-    int payment, price;
-    cout << "Payment >";
+    int payment, price, remain, charge;
+    cout << "料金 > ";
     cin >> payment;
-    cout << "Price >";
+    cout << "支払金額 > ";
     cin >> price;
+    charge = price - payment;
 
-    cout << price << "\n" << payment << endl;
+    array<int, 4> charge_by_coin = {0};
+    array<int, 4> coin_types = {500, 100, 50, 10};
+
+    for (auto [itr_k, itr_v] = pair{coin_types.begin(), charge_by_coin.begin()}; itr_k != coin_types.end(); ++itr_k, ++itr_v) {
+        *itr_v = charge / *itr_k;
+        charge -= *itr_k * *itr_v;
+    } 
+    
+    for (auto [itr_k, itr_v] = pair{coin_types.begin(), charge_by_coin.begin()}; itr_k != coin_types.end(); ++itr_k, ++itr_v) {
+        cout << *itr_k << "円硬貨: " << -*itr_v << "枚\n";
+    }
 }
